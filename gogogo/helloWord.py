0f911ed0e1e7cebde370e1115eb5462ba13cdd5d
chenxiaocehng <583188551@qq.com>
2019-12-18 11:13:47 +0800

    添加db的orm模型

--- gogogo/helloWord.py
+++ gogogo/helloWord.py
@@ -9,6 +9,9 @@ from django.http import HttpResponse
 """
 
 
+
+
+
 def hello(request):
     return HttpResponse("hello world ! ")
 
--- gogogo/settings.py
+++ gogogo/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'aekj)-10#%&@7d(^mbq+q&*430ys#pomt5_0(e72#ifrhoaqgr'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,7 +52,7 @@ ROOT_URLCONF = 'gogogo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+"/templates",],
+        'DIRS': [BASE_DIR + "/templates", ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,17 +67,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gogogo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 要连接的 数据库类型
+        'HOST': '127.0.0.1',  # 要连接的远程数据库的 ip地址
+        'PORT': '3339',  # 数据库连接端口，mysql默认3306
+        'USER': 'root',  # 数据库已有用户名
+        'PASSWORD': '123456',  # 数据库已有用户密码
+        'NAME': 'db_account',  # 要连接的 数据库名
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -99,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -112,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
